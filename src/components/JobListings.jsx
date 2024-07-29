1727782f2a573f4cf5b2861d7e3f04a7889b7021
Ali Bilal <MrAliBilal@outlook.com>
2024-07-29 17:03:29 +0500

    adding react spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from 'react';
 import JobListing from './JobListing'
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
 
@@ -8,8 +9,11 @@ const JobListings = ({isHome = false}) => {
 
     useEffect (() => {
         const fetchJobs = async () => {
+
+            const apiUrl = isHome ? 'http://localhost:5000/jobs?_limit=3' : 'http://localhost:5000/jobs'
+
             try{
-                const res = await fetch("http://localhost:5000/jobs");
+                const res = await fetch(apiUrl);
                 const data = await res.json();
                 setJobs (data);
             } catch {
@@ -28,12 +32,15 @@ const JobListings = ({isHome = false}) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {loading 
+                    ?  <Spinner loading={loading} />
+                    :                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {jobs.map((job) => (
                         <JobListing key={job.id} job={job}/>
                     ) )}
     
                 </div>
+                }
             </div>
         </section>
     )
